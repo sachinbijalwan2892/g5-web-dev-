ab3169d5ef9cf0fc9ff0e2cc2464b134f18f81e3
Sachin Bijalwan <sachinbijalwanrs200@gmail.com>
2025-07-08 23:00:04 +0530

    Meaningful message about what you changed

--- day2/script.js
+++ day2/script.js
@@ -1,1 +1,78 @@
-console.log("day 2")
+// let names=["Sachin", "Sahib", "Paras"]
+
+// let ans=names.map((name)=>{
+//     return "Mr. "+name;
+// })
+
+// console.log(ans);
+
+//let arr=[1,2,3,4,5,6,7,8,9];
+
+// let ans=arr.filter((num)=>{
+//     if(num%2===0)
+//     {
+//         return true;
+//     }
+//     else
+//     {
+//         return false;
+//     }
+// })
+
+// console.log(ans);
+
+// let ans=arr.reduce((acc,curr)=>{
+//     return acc+curr;
+// },10)
+
+// console.log(ans)
+
+// let ans1=arr.sort();
+
+// console.log(ans1)
+
+let a = [1,2,3,4,5,6,7,8,9]
+// a.sort((x,y)=>y-x);
+// console.log(a);
+
+// a.forEach((i)=>{
+//     console.log(i)
+// })
+
+// let obj={
+//     name:"Sachin",
+//     roll:2210992203,
+//     age:21
+// }
+
+// for(let key in obj)
+// {
+//     console.log(key+":"+obj[key]);
+// }
+
+// let name = "Sachin";
+
+// for(let c of name)
+// {
+//     console.log(c)
+// }
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+
+// function add(arr)
+// {
+//     let sum=0;
+
+//     arr.forEach((i)=>{
+//         sum+=i;
+//     })
+//     return sum;
+
+// }
+
+
+// let ans=add(arr);
+// console.log(ans)
+
+
+let arr=[]
---
+++ day3/scirpt.js
@@ -0,0 +1,72 @@
+
+// // add(2,3);
+// // function add(a,b)
+// // {
+// //     console.log(a+b);
+// // }
+
+
+
+// // console.log(name)
+// // var name="Sachin";
+
+
+
+// // console.log(name)
+// // let name="Sachin";
+
+
+// // class Human{
+// //     //properties
+// //     age=21;
+// //     #wt=75;//private
+// //     ht=182;
+
+// //     walking()
+// //     {
+// //         console.log("I am walking");
+// //     }
+
+// //     running(){
+// //         console.log("I am running");
+// //     }
+// //     get modweight()
+// //     {
+// //         return this.#wt;
+// //     }
+// //     set weight(val)
+// //     {
+// //         this.#wt=val;
+// //     }
+// // }
+
+// // let human1=new Human();
+
+
+// // human1.weight=80;
+// // console.log(human1.modweight);
+
+// // human1.running();
+ 
+// //Object Cloning
+
+// let src={
+//     age:21,
+//     wt:75,
+//     ht:182
+// }
+
+// // let dest={...src};
+
+// // src.age=19
+// // console.log(src);
+// // console.log(dest)
+
+// let dest=Object.assign({},src);
+
+// console.log(src);
+// console.log(dest)
+
+
+//error handling
+
