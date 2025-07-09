1830913ec763fdced8c00be920e6e43d7954532a
Sachin Bijalwan <sachinbijalwanrs200@gmail.com>
2025-07-09 10:53:48 +0530

    day 3 done

---
+++ day4/error.js
@@ -0,0 +1,17 @@
+//compile time error
+//console.log(x;
+
+
+//run time error
+//console.log(x)
+
+
+//handling
+try{
+    console.log("Sachin");
+    console.log(a);
+    console.log("Bijalwan");
+}
+catch(error){
+     console.log("I am inside catch block and this is your error", error);
+}
